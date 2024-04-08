956305efd427f74f11e0524cd96e94e8db5f863e
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-04-08 08:25:30 +0700

    release v0.0.2

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name Firepoker Cheat
 // @namespace https://santus.dev/
-// @version 0.0.1
 // @description Reveal Points for Firepoker
 // @author Krisantus Wanandi (https://github.com/krisantuswanandi)
 // @match https://firepoker.app/**
@@ -9,6 +8,7 @@
 // @run-at document-body
 // @license MIT
 // @supportURL https://github.com/krisantuswanandi/firepoker-cheat
+// @version 0.0.2
 // ==/UserScript==
 
 (function () {
@@ -18,14 +18,42 @@
 // src/style.ts
 function injectStyle() {
   const style = document.createElement("style");
-  style.innerHTML = `.card.ng-binding { display: block !important } .card.card-facedown { display: none !important }`;
+  style.innerHTML = `
+.cheat-enabled {
+  .card.ng-binding {
+    display: block !important
+  }
+  .card.card-facedown {
+    display: none !important
+  }
+}`;
   document.head.appendChild(style);
 }
 
 // src/index.ts
+var isFocusOnInput = function(event) {
+  return INPUTS.includes(event.target.getAttribute("ng-model") || "");
+};
+var isShortcut = function(event) {
+  return event.key.toLowerCase() === SHORTCUT_KEY;
+};
+var handleShortcut = function(event) {
+  if (isFocusOnInput(event) || !isShortcut(event))
+    return;
+  if (event.type === "keydown") {
+    document.body.classList.add(CLASS_NAME);
+  } else if (event.type === "keyup") {
+    document.body.classList.remove(CLASS_NAME);
+  }
+};
 var init = function() {
   injectStyle();
+  window.addEventListener("keydown", handleShortcut);
+  window.addEventListener("keyup", handleShortcut);
 };
+var INPUTS = ["game.estimate.title", "game.estimate.notes"];
+var SHORTCUT_KEY = "x";
+var CLASS_NAME = "cheat-enabled";
 init();
 
 })();
